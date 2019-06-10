28e68f81f60f62eecbf1b437c755bf4b4268aa4c
Riad Bensalem <fr_bensalem@esi.dz>
2019-06-10 23:45:02 +0200

    graph added

--- centralApp/views.py
+++ centralApp/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def hello(request):
+   return render(request, "bootstrap/index.html", {})
