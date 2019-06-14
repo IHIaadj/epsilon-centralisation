0c4edab401aa3eef41d107af5ea2d9a945a838b8
Hadjer <fh_benmeziane@esi.dz>
2019-06-14 14:57:29 +0200

    Add files via upload

--- centralApp/views.py
+++ centralApp/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from Backend import backend 
 
 def hello(request):
-   return render(request, "bootstrap/index.html", {})
+   IPs = []
+   IPs.append("192.168.1.41")
+   IPs.append("192.168.1.41")
+
+   disk = []
+   disk.append(backend.getDiskUsage("192.168.1.41"))
+   disk.append(backend.getDiskUsage("192.168.1.41"))
+   ram = []
+   ram.append(backend.getRamUsage("192.168.1.41"))
+   ram.append(backend.getRamUsage("192.168.1.41"))
+   return render(request, "bootstrap/index.html", {'disk' : disk, 'ram': ram, "IPs" : IPs})
